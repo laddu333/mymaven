f1be37556c17b8a68fdde6ba8770e359edd8ccc5
laddu333 <mailtosiva333@gmail.com>
2019-04-07 01:20:06 +0530

    message

---
+++ src/main/java/com/mymaven/pro/App.java
@@ -0,0 +1,13 @@
+package com.mymaven.pro;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
